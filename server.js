e64eb7cf9fd3a99d9e517ba1915b70850edb47f0
Osman Zakir <osmanzakir90@hotmail.com>
2021-05-30 17:27:40 +0500

    switched to arrow functions and used template literals where applicable

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //Index page (static HTML)
 app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  .get((req, res) => {
+    res.sendFile(`${process.cwd()}/views/index.html`);
   });
 
 //For FCC testing purposes
@@ -32,23 +32,23 @@ fccTestingRoutes(app);
 apiRoutes(app);  
     
 //404 Not Found Middleware
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.status(404)
-    .type('text')
-    .send('Not Found');
+    .type("text")
+    .send("Not Found");
 });
 
 //Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+  if(process.env.NODE_ENV === "test") {
+    console.log("Running Tests...");
+    setTimeout(() => {
       try {
         runner.run();
       } catch(e) {
         let error = e;
-          console.log('Tests are not valid:');
+          console.log("Tests are not valid:");
           console.log(error);
       }
     }, 1500);
