e83dfa7787dcc0bd98899fa9eae36c211c583b91
Osman Zakir <osmanzakir90@hotmail.com>
2021-06-01 21:20:45 +0500

    added tests for reading a decimal number input

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -8,14 +8,14 @@ let convertHandler = new ConvertHandler();
 suite("Unit Tests", () => {
   describe("Do conversions for numbers between metric and imperial", () => {
     describe("Get a number", () => {
-      it("should read a whole number input", () => {
+      it("should correctly read a whole number input", () => {
         const input1 = "3mi";
         const input2 = "30km";
         const input3 = "10L";
         const input4 = "100kg";
         const input5 = "20gal";
         const input6 = "40lbs";
-        
+
         const num1 = convertHandler.getNum(input1);
         const num2 = convertHandler.getNum(input2);
         const num3 = convertHandler.getNum(input3);
@@ -29,6 +29,29 @@ suite("Unit Tests", () => {
         expect(num4).to.equal(100);
         expect(num5).to.equal(20);
         expect(num6).to.equal(40);
+      });
+
+      it("should correctly read a decimal number input", () => {
+        const input1 = "3.1mi";
+        const input2 = "30.43km";
+        const input3 = "10.2354L";
+        const input4 = "100.2345kg";
+        const input5 = "20.3456gal";
+        const input6 = "40.5678lbs";
+
+        const num1 = convertHandler.getNum(input1);
+        const num2 = convertHandler.getNum(input2);
+        const num3 = convertHandler.getNum(input3);
+        const num4 = convertHandler.getNum(input4);
+        const num5 = convertHandler.getNum(input5);
+        const num6 = convertHandler.getNum(input6);
+  
+        expect(num1).to.equal(3.1);
+        expect(num2).to.equal(30.43);
+        expect(num3).to.equal(10.2354);
+        expect(num4).to.equal(100.2345);
+        expect(num5).to.equal(20.3456);
+        expect(num6).to.equal(40.5678);
       });
     });
   });
