c3f50c85c8998b02ec7de061ebac57b0648b6ddc
Osman Zakir <osmanzakir90@hotmail.com>
2021-07-06 23:09:22 +0500

    put only calls to getNum and getUnit inside try-catch block

--- routes/api.js
+++ routes/api.js
@@ -8,22 +8,27 @@ module.exports = (app) => {
   let convertHandler = new ConvertHandler();
 
   app.route("/api/convert").get((req, res) => {
+    const inputStr = req.query.input;
+    let inputNumber;
+    let inputUnit;
+
     try {
-      const inputStr = req.query.input;
-      const inputNumber = convertHandler.getNum(inputStr);
-      const inputUnit = convertHandler.getUnit(inputStr);
-      const returnUnit = convertHandler.getReturnUnit(inputUnit);
-      const result = convertHandler.convert(inputNumber, inputUnit);
-      const fullResultStr = convertHandler.getString(inputNumber, inputUnit, result, returnUnit);
-      return res.json({
-        initNum: inputNumber,
-        initUnit: inputUnit,
-        returnNum: Number(result),
-        returnUnit: returnUnit,
-        string: fullResultStr
-      });
+      inputNumber = convertHandler.getNum(inputStr);
+      inputUnit = convertHandler.getUnit(inputStr);
     } catch (error) {
       return res.json(error.message);
     }
+
+    const returnUnit = convertHandler.getReturnUnit(inputUnit);
+    const result = convertHandler.convert(inputNumber, inputUnit);
+    const fullResultStr = convertHandler.getString(inputNumber, inputUnit, result, returnUnit);
+
+    return res.json({
+      initNum: inputNumber,
+      initUnit: inputUnit,
+      returnNum: Number(result),
+      returnUnit: returnUnit,
+      string: fullResultStr
+    });
   });
 };
